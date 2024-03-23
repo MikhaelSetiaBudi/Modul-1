7a46b096d5806fc2f6a30844e59f168f33644ed9
MikhaelSetiaBudi <mikhaelbudi78@gmail.com>
2024-03-24 01:03:13 +0700

    Upload Modul 1

--- Unguided3.cpp
+++ Unguided3.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <map>
 
 using namespace std;
 
-// Fungsi yang digunakan untuk menggandakan elemen
-int ganda(int x) {
-    return x * 2;
-}
-
 int main() {
-    // Array
-    int array[] = {11, 15, 20, 25, 30, 35}; 
+    string kata;
+    cout << "Masukkan sebuah kalimat: ";
+    getline(cin, kata); // Membaca seluruh baris kalimat dari input
 
-    // Vektor yang digunakan untuk menyimpan hasil transformasi
-    vector<int> hasil;
+    map<char, int> frekuensi;
 
-    // menggunakan fungsi map dan transformasi
-    transform(begin(array), end(array), back_inserter(hasil), ganda); 
-
-    // Menampilkan hasil array dan hasil transformasi
-    cout << "Array: ";
-    for (int i : array) {
-        cout << i << " ";
+    // Menghitung frekuensi kemunculan setiap karakter dalam kalimat
+    for (char karakter : kata) {
+        if (isalpha(karakter)) { 
+            karakter = tolower(karakter); 
+            frekuensi[karakter]++;
+        }
     }
-    cout << endl;
 
-    cout << "Hasil transformasi: ";
-    for (int i : hasil) {
-        cout << i << " ";
+    // Menampilkan frekuensi kemunculan setiap karakter
+    cout << "Frekuensi kemunculan setiap karakter dalam kalimat: "<< endl;
+    for (auto& pasangan : frekuensi) {
+        cout << "'" << pasangan.first << "': " << pasangan.second << endl;
     }
-    cout << endl;
 
     return 0;
 }
-
-
-// Mikhael Setia Budi
-// 2311110033
-//copyright@MikhaelS.B  
